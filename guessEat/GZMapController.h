aeb984a0d8992bcdd6bb7aaf6919ace5c57e106c
CHASTMASTER <zhengwuqiu@hotmail.com>
2013-09-11 09:27:23 +0800

    tet edition for map

---
+++ guessEat/GZMapController.h
@@ -0,0 +1,16 @@
+//
+//  GZMapController.h
+//  guessEat
+//
+//  Created by Stephen Zheng on 10/09/13.
+//  Copyright (c) 2013 net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GZMapController : UIViewController
+
+
+-(IBAction)goToGamingpageCrt;
+
+@end
